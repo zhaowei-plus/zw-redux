2113fb23dfb2d1cbf5c884c9d6361c4cd9fc4c16
wb-zhaowei@cai-inc.com <1215058679@qq.com>
2019-06-12 20:32:06 +0800

    [feat: init]

--- src/main.js
+++ src/main.js
@@ -4,9 +4,22 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { sayHello } from 'yage-npm';
+import regexpStore  from 'zcy-regexpstore-front';
+
 import App from './components/App';
 import reducer from './reducers';
 
+console.log(sayHello());
+console.log(regexpStore)
+
+/**
+ * 创建一个Redux Store，用来存放本应用中所有的state
+ * 注：一个应用中只能有一个Store
+ *
+ * createStore(reducer, [initState, enhancer])
+ *
+ * */
 const store = createStore(reducer);
 
 ReactDOM.render(
